6cd9733e2ff652cc6779bd580b7665551686431e
olaskoog <ola@olaskoog.se>
2015-06-17 14:36:54 +0200

    Update RBAC.ps1

--- RBAC.ps1
+++ RBAC.ps1
@@ -4,17 +4,17 @@
 Get-AzureRoleAssignment
 
 #Check existing role assignments for a particular role definition, at a particular scope to a particular user
-Get-AzureRoleAssignment -ResourceGroupName RGTemp -Mail olskoog@microsoft.com -RoleDefinitionName Owner
+Get-AzureRoleAssignment -ResourceGroupName RGTemp -Mail XXX -RoleDefinitionName Owner
 
 #List the supported role definitions
 Get-AzureRoleDefinition
 
 #This will create a role assignment at a resource group level
-New-AzureRoleAssignment -Mail olskoog@microsoft.com -RoleDefinitionName Contributor -ResourceGroupName RGTemp
+New-AzureRoleAssignment -Mail XXX -RoleDefinitionName Contributor -ResourceGroupName XXX
 
 #This will create a role assignment for a group at a resource group level
-New-AzureRoleAssignment -ObjectID <group object ID> -RoleDefinitionName Reader -ResourceGroupName RGTemp
+New-AzureRoleAssignment -ObjectID XXX -RoleDefinitionName Reader -ResourceGroupName XXX
 
 #This will create a role assignment at a resource level
 $resources = Get-AzureResource
-New-AzureRoleAssignment -Mail olskoog@microsoft.com -RoleDefinitionName Owner -Scope $resources[0].ResourceId
+New-AzureRoleAssignment -Mail XXX -RoleDefinitionName Owner -Scope $resources[0].ResourceId
